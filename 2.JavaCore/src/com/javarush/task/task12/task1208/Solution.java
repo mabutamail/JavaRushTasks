215b9db5d5d10290c76bd4792bbc6e5f7a5cc004
mabuta <mabutamail@gmail.com>
2019-04-10 22:49:35 +0300

    1
    Signed-off-by: mabuta <mabutamail@gmail.com>

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+Написать пять методов print с разными параметрами.
+
+
+Требования:
+1. Программа не должна выводить текст на экран.
+2. Класс Solution должен содержать шесть методов.
+3. Класс Solution должен содержать статический метод main().
+4. Класс Solution должен содержать пять статических методов print() с разными параметрами.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+
+    public static void print(int i){}
+    public static void print(Integer i){}
+    public static void print(String s){}
+    public static void print(Double d){}
+    public static void print(Long i){}
+}
