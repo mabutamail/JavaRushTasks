5da5e3ab2fae0a7a84c7b40c4e5db4f4940bd79d
mabuta <mabutamail@gmail.com>
2019-04-16 19:33:50 +0300

    1
    Signed-off-by: mabuta <mabutamail@gmail.com>

--- 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
@@ -1,9 +1,9 @@
 package com.javarush.task.task13.task1318;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
-import java.io.FileInputStream;
+import java.util.stream.Stream;
 
 /* 
 Чтение файла
@@ -14,13 +14,42 @@ import java.io.FileInputStream;
 
 Требования:
 1. Программа должна считывать c консоли имя файла.
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            FileInputStream fileName = new FileInputStream(reader.readLine());
 2. Программа должна выводить на экран содержимое файла.
+            while (fileName.available() > 0) {
+                System.out.print((char) fileName.read());
+            }
 3. Поток чтения из файла (FileInputStream) должен быть закрыт.
+            fileName.close();
 4. BufferedReader также должен быть закрыт.
+            reader.close();
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            FileInputStream fileName = new FileInputStream(reader.readLine());
+            while (fileName.available() > 0) {
+                System.out.print((char) fileName.read());
+            }
+            System.out.println();
+            fileName.close();
+
+//            Stream                        интерфейс
+//            InputStreamReader             класс
+//            InputStream                   абстрактный класс
+//                  FileInputStream
+//            Reader                        абстрактный класс
+//                  BufferedReader
+//            FilterInputStream
+//                  BufferedInputStream
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        reader.close();
     }
 }
---
+++ 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task13.task1319;
+
+import java.io.*;
+
+/* 
+Писатель в файл с консоли
+1. Прочесть с консоли имя файла.
+2. Считывать строки с консоли, пока пользователь не введет строку "exit".
+3. Вывести абсолютно все введенные строки в файл, каждую строчку с новой строки.
+
+
+Требования:
+1. Программа должна считывать c консоли имя файла.
+2. Создай и используй объект типа BufferedWriter.
+3. Программа не должна ничего читать из файловой системы.
+4. Программа должна считывать строки с консоли, пока пользователь не введет строку "exit".
+5. Программа должна записать абсолютно все введенные строки (включая "exit")
+в файл, каждую строчку с новой строки.
+6. Метод main должен закрывать объект типа BufferedWriter после использования.
+7. Метод main не должен выводить данные на экран.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        // напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream fileName = new FileInputStream(reader.readLine());
+
+        while (fileName.available() > 0) {
+            System.out.print((char) fileName.read());
+        }
+
+        fileName.close();
+        reader.close();
+    }
+}
