616042107e7511ae0c8b5748293350caaea4aa4b
mabuta <mabutamail@gmail.com>
2019-05-19 13:33:51 +0300

    1
    Signed-off-by: mabuta <mabutamail@gmail.com>

--- 2.JavaCore/src/com/javarush/task/task15/task1522/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1522/Solution.java
@@ -62,6 +62,7 @@ public class Solution {
     }
 
     public static Planet thePlanet;
+
     //add static block here - добавьте статический блок тут
     static {
         readKeyFromConsoleAndInitPlanet();
@@ -69,10 +70,14 @@ public class Solution {
 
     public static void readKeyFromConsoleAndInitPlanet() {
         // implement step #5 here - реализуйте задание №5 тут
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-
-
-
+        try {
+            String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
+            if (s.equals(Planet.EARTH)) thePlanet = Earth.getInstance();
+            else if (s.equals(Planet.MOON)) thePlanet = Moon.getInstance();
+            else if (s.equals(Planet.SUN)) thePlanet = Sun.getInstance();
+            else thePlanet = null;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
