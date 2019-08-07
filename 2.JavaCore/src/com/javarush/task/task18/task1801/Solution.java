9cd222c81e916c3595a69c9af9a912e77da90449
mabuta <mabutamail@gmail.com>
2019-08-07 11:49:18 +0300

    1
    Signed-off-by: mabuta <mabutamail@gmail.com>

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1801/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task18.task1801;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+/* 
+Максимальный байт
+Ввести с консоли имя файла.
+Найти максимальный байт в файле, вывести его на экран.
+Закрыть поток ввода-вывода.
+
+
+Требования:
+1. Программа должна считывать имя файла с консоли.
+2. Для чтения из файла используй поток FileInputStream.
+3. В консоль должен выводиться максимальный байт, считанный из файла.
+4. Поток чтения из файла должен быть закрыт.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        FileInputStream inputStream = new FileInputStream
+            (new BufferedReader(new InputStreamReader(System.in)).readLine());
+        int max = 0, i;
+        while (inputStream.available() > 0)
+            if ((i = inputStream.read()) > max)
+                max = i;
+        inputStream.close();
+        System.out.println(max);
+
+    }
+}
