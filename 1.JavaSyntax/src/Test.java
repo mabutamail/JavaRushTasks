2cf1eaa10e5ef713d4bfa916a7bac144c1edc8f9
mabutamail <mabutamail@gmail.com>
2020-01-06 12:53:40 +0300

    1
    Signed-off-by: mabutamail <mabutamail@gmail.com>

--- 1.JavaSyntax/src/Test.java
+++ 1.JavaSyntax/src/Test.java
@@ -1,2 +1,9 @@
 public class Test {
+    public static void main(String[] args) {
+        //write your code here
+        for (int i = 0; i < 5; i++)
+        {
+            System.out.println("Ellie is clever");
+        }
+    }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
@@ -1,9 +1,9 @@
 package com.javarush.task.task23.task2312;
 
 public class SnakeSection {
-
     private int x;
     private int y;
+
 
     public SnakeSection(int x, int y) {
         this.x = x;
@@ -18,4 +18,23 @@ public class SnakeSection {
         return y;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SnakeSection that = (SnakeSection) o;
+
+        if (x != that.x) return false;
+        if (y != that.y) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
 }
