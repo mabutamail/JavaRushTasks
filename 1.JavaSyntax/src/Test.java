e146cd56c1685ec7b1d305f9a0dcb45501146654
mabutamail <mabutamail@gmail.com>
2020-01-10 22:28:43 +0300

    1
    Signed-off-by: mabutamail <mabutamail@gmail.com>

--- 1.JavaSyntax/src/Test.java
+++ 1.JavaSyntax/src/Test.java
@@ -1,9 +1,6 @@
-public class Test {
-    public static void main(String[] args) {
-        //write your code here
-        for (int i = 0; i < 5; i++)
-        {
-            System.out.println("Ellie is clever");
-        }
+class Kata {
+    public static String numberToString(int num) {
+        // Return a string of the number here!
+        return num + "";
     }
 }
